69ed11e67525df00ed8cc9af9441204318027663
Abhijeet <shindeabhijeet552@gmail.com>
2024-06-25 20:43:55 +0530

    Added User Regs and Login

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -1,7 +1,8 @@
 import User from "../Model/user.js";
 
 export const registerUser = () =>{
-	const user = {
+	try{
+        const user = {
         name,
         email,
         password,
@@ -15,4 +16,36 @@ export const registerUser = () =>{
         res.status(201).json({
             message: 'User created'})
         })
+    }catch(error){
+        res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+}
+
+export const loginUser = () =>{
+    try{
+        const {email, password} = req.body
+        const isUser = User.findOne(email | email);
+        if(isUser){
+            if(isUser.password === password){
+                res.status(200).json({
+                    message: 'Login successful'
+                })
+            }else{
+                res.status(401).json({
+                    message: 'Invalid credentials'
+                })
+            }
+        }
+        else{
+            res.status(404).json({
+                message: 'User not found'
+            })
+        }
+    }catch(error){
+        res.status(500).json({
+            message: ' Internal Server Error'
+        })
+    }
 }
